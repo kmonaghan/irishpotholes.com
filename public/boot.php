a9d53fd5c962eef6ee8a45bbac9b0250edd013dd
kmonaghan <karl.t.monaghan@gmail.com>
2013-03-24 22:38:18 +0100

    right mysql variables

--- public/boot.php
+++ public/boot.php
@@ -1,10 +1,11 @@
 <?php
 date_default_timezone_set('Europe/Dublin');
 
-define('HOST', 'localhost');
-define('DATABASE', 'potholes');
-define('DB_USER', 'USERNAME');
-define('DB_PASSWORD', 'PASSWORD');
+define('MYSQLI_HOST', 'localhost');
+define('MYSQL_PORT', 3306);
+define('MYSQL_DATABASE', 'potholes');
+define('MYSQL_USER', 'USERNAME');
+define('MYSQL_PASSWORD', 'PASSWORD');
 
 function __autoload($class_name) {
 	include '/home/irishpotholes.com/Classes/' . strtolower($class_name) . '.class.php';
