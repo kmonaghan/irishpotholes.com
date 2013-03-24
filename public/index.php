5faf4eacfe6e4f23f55d836dff6636a117335f6b
kmonaghan <karl.t.monaghan@gmail.com>
2013-03-24 17:37:07 +0100

    getting file uploading to work

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
-
+<?php 
+include 'boot.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -13,80 +15,7 @@
 	<link href="/css/prettyCheckable.css" rel="stylesheet">
 	<link href="/css/datepicker.css" rel="stylesheet">
 	<link href="/css/fineuploader-3.3.0.css" rel="stylesheet">
-
-    <style type="text/css">
-
-      /* Sticky footer styles
-      -------------------------------------------------- */
-
-      html,
-      body {
-        height: 100%;
-        /* The html and body elements cannot have any padding or margin. */
-      }
-
-      /* Wrapper for page content to push down footer */
-      #wrap {
-        min-height: 100%;
-        height: auto !important;
-        height: 100%;
-        /* Negative indent footer by it's height */
-        margin: 0 auto -60px;
-      }
-
-      /* Set the fixed height of the footer here */
-      #push,
-      #footer {
-        height: 60px;
-      }
-      #footer {
-        background-color: #f5f5f5;
-      }
-
-      /* Lastly, apply responsive CSS fixes as necessary */
-      @media (max-width: 767px) {
-        #footer {
-          margin-left: -20px;
-          margin-right: -20px;
-          padding-left: 20px;
-          padding-right: 20px;
-        }
-      }
-
-
-
-      /* Custom page CSS
-      -------------------------------------------------- */
-      /* Not required for template or sticky footer method. */
-
-      .container {
-        width: auto;
-        max-width: 680px;
-      }
-      .container .credit {
-        margin: 20px 0;
-      }
-
-	/* Fine Uploader
-      -------------------------------------------------- */
-      .qq-upload-list {
-        text-align: left;
-      }
- 
-      /* For the bootstrapped demos */
-      li.alert-success {
-        background-color: #DFF0D8;
-      }
- 
-      li.alert-error {
-        background-color: #F2DEDE;
-      }
- 
-      .alert-error .qq-upload-failed-text {
-        display: inline;
-      }
-
-    </style>
+	<link href="/css/pothole.css" rel="stylesheet">
     <link href="/css/bootstrap-responsive.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
@@ -109,54 +38,62 @@
     <div id="wrap">
 
       <!-- Begin page content -->
-      <div class="container">
+      <div class="container-fluid">
         <div class="page-header">
           <h1>Irish Potholes</h1>
         </div>
         <p class="lead">Irish Potholes</p>
         <p>Some more details</p>
 
-	<form method="POST" action="">
-		<input type="hidden" name="lat" value="" />
-		<input type="hidden" name="lng" value="" />
-		<fieldset>
-			<legend>Pothole Details</legend>
-			<label>Your Email</label>
-			<input class="span3" type="email" required>
-    			<label>Where's the pothole? </label>
-        		<input class="span3" type="text" id="addresspicker_map" />
-        		<div style="width:300px;height:300px;margin-top:20px">
-            			<div id="map_canvas" style="width:100%; height:100%"></div>
-            			<div id="location" class=""></div>
-        		</div>
-			<label>When did you encounter it?</label>
-			<div class="input-append date datepicker" data-date="<?php echo Date('d/m/Y'); ?>" data-date-format="dd/mm/yyyy">
-    				<input size="16" type="text" value="<?php echo Date('d/m/Y'); ?>" readonly><span class="add-on"><i class="icon-calendar"></i></span>
-			</div>
-			<div class="input-wrapper">
-              			<label>How Bad? 1 (being a tooth chipper) to 5 (being the Grand Canyon)</label>
-              			<input type="radio" class="pothole-radio" value="1" id="Test3_0" name="Test3" data-label="1" checked data-customclass="margin-right"/>
-              			<input type="radio" class="pothole-radio" value="2" id="Test3_1" name="Test3" data-label="2" />
-            			<input type="radio" class="pothole-radio" value="3" id="Test3_2" name="Test3" data-label="3" />
-				<input type="radio" class="pothole-radio" value="4" id="Test3_3" name="Test3" data-label="4" />
-				<input type="radio" class="pothole-radio" value="5" id="Test3_4" name="Test3" data-label="5" />
-			</div>
-			<div class="input-wrapper" style="height:46px;">
-				<div id="bootstrapped-fine-uploader"></div>
-			</div>
-			<div class="form-actions">
-				<button type="submit" class="btn">Add Pothole</button>
-			</div>
-		</fieldset>
-	</form>
+        <div class="row-fluid">
+			<form method="POST" action="">
+				<input type="hidden" name="lat" value="" />
+				<input type="hidden" name="lng" value="" />
+
+					<legend>Pothole Details</legend>
+					<div class="input-wrapper">
+						<label>Your Email</label>
+						<input type="email" id="report-email" name="report-email" required>
+					</div>
+					<div class="input-wrapper">
+		    			<label>Where's the pothole? </label>
+		        		<input type="text" id="addresspicker_map" />
+		        		<div style="width:280px;height:300px;margin-top:20px">
+		            			<div id="map_canvas" style="width:100%; height:100%"></div>
+		            			<div id="location" class=""></div>
+		        		</div>
+		        	</div>
+		        	<div class="input-wrapper">
+						<label>When did you encounter it?</label>
+						<div class="input-append date datepicker" data-date="<?php echo Date('d/m/Y'); ?>" data-date-format="dd/mm/yyyy">
+		    				<input size="16" type="text" value="<?php echo Date('d/m/Y'); ?>" readonly><span class="add-on"><i class="icon-calendar"></i></span>
+						</div>
+					</div>
+					<div class="input-wrapper">
+		              			<label>How Bad? 1 (being a tooth chipper) to 5 (being the Grand Canyon)</label>
+		              			<input type="radio" class="pothole-radio" value="1" id="bad-0" name="bad" data-label="1" checked data-customclass="margin-right"/>
+		              			<input type="radio" class="pothole-radio" value="2" id="bad-1" name="bad" data-label="2" />
+		            			<input type="radio" class="pothole-radio" value="3" id="bad-2" name="bad" data-label="3" />
+								<input type="radio" class="pothole-radio" value="4" id="bad-3" name="bad" data-label="4" />
+								<input type="radio" class="pothole-radio" value="5" id="bad-4" name="bad" data-label="5" />
+					</div>
+					<div class="input-wrapper">
+						<div id="bootstrapped-fine-uploader"></div>
+					</div>
+					<div class="form-actions">
+						<button type="submit" class="btn">Add Pothole</button>
+					</div>
+
+			</form>
+		</div>
       </div>
 
       <div id="push"></div>
     </div>
 
     <div id="footer">
-      <div class="container">
-        <p class="muted credit">Built by <a href="http://karlmonaghan.com">Karl Monaghan</a> from an idea by <a href="https://twitter.com/AaronMcAllorum">Aaron McAllorum</a>.</p>
+      <div class="container-fluid">
+        <p class="muted credit">Built by <a href="http://karlmonaghan.com">Karl Monaghan</a> from an idea by <a href="https://twitter.com/AaronMcAllorum">Aaron McAllorum</a>.&nbsp;|&nbsp;<a href="http://www.karlmonaghan.com/contact">Get in touch</a>&nbsp;|&nbsp;<a href="http://www.karlmonaghan.com/">About</a>&nbsp;|&nbsp;<a href="https://github.com/kmonaghan/irishpotholes.com">Code</a></p>
       </div>
     </div>
 
@@ -178,28 +115,49 @@
 	ga('send', 'pageview');
 
 	function createUploader() {
-        	var uploader = new qq.FineUploader({
-		element: document.getElementById('bootstrapped-fine-uploader'),
+		var uploader = new qq.FineUploader({
+			element: document.getElementById('bootstrapped-fine-uploader'),
           	request: {
-            		endpoint: 'server/handleUploads'
+            		endpoint: 'image_uploader.php'
+          	},
+          	validation: {
+          		allowedExtensions: ['png', 'PNG', 'jpg', 'JPG', 'jpeg', 'JPEG', 'gif', 'GIF']
           	},
           	text: {
-            		uploadButton: '<div><i class="icon-upload icon-white"></i> Upload a picture</div>'
+            		uploadButton: '<i class="icon-upload icon-white"></i> Upload a picture'
           	},
-          	template: '<div class="qq-uploader span3">' +
-                      '<pre class="qq-upload-drop-area span3"><span>{dragZoneText}</span></pre>' +
+          	template: '<div class="qq-uploader">' +
+                      '<pre class="qq-upload-drop-area"><span>{dragZoneText}</span></pre>' +
                       '<div class="qq-upload-button btn btn-success" style="width: auto;">{uploadButtonText}</div>' +
                       '<span class="qq-drop-processing"><span>{dropProcessingText}</span><span class="qq-drop-processing-spinner"></span></span>' +
                       '<ul class="qq-upload-list" style="margin-top: 10px; text-align: center;"></ul>' +
                     '</div>',
-		classes: {
+			classes: {
             		success: 'alert alert-success',
             		fail: 'alert alert-error'
-          	}
+          	},
+          	callbacks: {
+                onComplete: function(id, name, response) {
+					if (response.success)
+					{
+                    	console.log('filename: ' + response.filename);
+					}
+                }
+            }
         });
       }
 
 	$(function() {
+		if(navigator.geolocation) 
+		{
+			navigator.geolocation.getCurrentPosition(function(position) {
+				var pos = new google.maps.LatLng(position.coords.latitude,
+									 position.coords.longitude);
+
+				map.setCenter(pos);
+			});
+		}
+		
 		createUploader();
 
 		$('.datepicker').datepicker({
@@ -213,27 +171,27 @@
 
 		var addresspickerMap = $( "#addresspicker_map" ).addresspicker(
         	{
-			regionBias: "ie",
-            		map:      "#map_canvas",
-            		typeaheaddelay: 1000,
-            		mapOptions: {
-                		zoom:16,
-                		center: new google.maps.LatLng(52.5122, 13.4194)
-            		}
+				regionBias: "ie",
+				map:      "#map_canvas",
+				typeaheaddelay: 1000,
+				mapOptions: {
+					zoom:16,
+					center: new google.maps.LatLng(52.5122, 13.4194)
+				}
 		});
 
-        	addresspickerMap.on("addressChanged", function(evt, address) {
-           		console.dir(address);
+		addresspickerMap.on("addressChanged", function(evt, address) {
+ 			console.dir(address);
 			console(address.LatLng);
-        	});
+		});
         
 		addresspickerMap.on("positionChanged", function(evt, markerPosition) {
-            		markerPosition.getAddress( function(address) {
-                		if (address) {
-                    			$( "#addresspicker_map").val(address.formatted_address);
-                		}
-            		})
-        	});
+			markerPosition.getAddress( function(address) {
+				if (address) {
+					$( "#addresspicker_map").val(address.formatted_address);
+				}
+			});
+		});
 	});
 </script>
   </body>
