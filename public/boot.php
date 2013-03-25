85c5cab20b78a1c1a2ee5a11a4658c38ec8a8185
kmonaghan <karl.t.monaghan@gmail.com>
2013-03-26 00:14:39 +0100

    added defines for image uploads

--- public/boot.php
+++ public/boot.php
@@ -4,8 +4,11 @@ date_default_timezone_set('Europe/Dublin');
 define('MYSQLI_HOST', 'localhost');
 define('MYSQL_PORT', 3306);
 define('MYSQL_DATABASE', 'potholes');
-define('MYSQL_USER', 'USERNAME');
+define('MYSQL_USER', 'USER');
 define('MYSQL_PASSWORD', 'PASSWORD');
+
+define('MAX_DIMENSION', 1024);
+define('UPLOAD_DIR', '/home/irishpotholes.com/public/uploads');
 
 function __autoload($class_name) {
 	include '/home/irishpotholes.com/Classes/' . strtolower($class_name) . '.class.php';
