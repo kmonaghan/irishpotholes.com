08a6efeb5d8c5ed5b3e8e6de49f798773735dffa
kmonaghan <karl.t.monaghan@gmail.com>
2013-03-28 17:41:12 +0100

    added links to pothole images, started on fixing pagination

--- public/potholes.php
+++ public/potholes.php
@@ -2,7 +2,7 @@
 include 'boot.php';
 include 'header.php';
 
-$page = (isset($_GET['page']) && ($_GET['page'] > 1)) ? $_GET['page'] : 0;
+$page = (isset($_GET['page']) && ($_GET['page'] > 1)) ? $_GET['page'] - 1 : 0;
 $perPage = (isset($_GET['count']) && ($_GET['count'] > 0) && ($_GET['count'] <= 20)) ? $_GET['count'] : 20;
 
 $mapper = new PotholeMapper();
@@ -18,11 +18,14 @@ if ($potholes)
 	{
 		$image = $pothole->getFirstImage();
 ?>
-	<li class="span4">
+	<li>
 		<div class="thumbnail">
-			<img data-src="holder.js/300x300" alt="" src="/uploads/300x300_<?php echo $image->get('filename');?>">
+			<div class="pagination-centered">
+				<a href="/uploads/<?php echo $image->get('filename');?>"><img data-src="holder.js/300x300" alt="" src="/uploads/300x300_<?php echo $image->get('filename');?>"></a>
+			</div>
 			<h5>Reported by <?php echo $pothole->get('nickname')?> on the <?php echo date('d/m/Y', $pothole->get('report_date')); ?></h5>
 			<p><?php echo $pothole->get('description')?></p>
+			<a href="/pothole.php?pothole_id=<?php echo $pothole->get('pothole_id'); ?>" class="btn btn-primary">View</a>
 		</div>
 	</li>
 <?php
@@ -35,13 +38,17 @@ if ($potholes)
 ?>
 <div class="pagination pagination-centered">
   <ul>
-    <li><a href="/potholes.php?page=">Prev</a></li>
+<?php if ($pagination['currentPage'] > 2) {?>
+    <li><a href="/potholes.php?page=<?php echo ($pagination['currentPage'] - 1); ?>">Prev</a></li>
+<?php }?>
     <li><a href="/potholes.php?page=">1</a></li>
     <li><a href="/potholes.php?page=">2</a></li>
     <li><a href="/potholes.php?page=">3</a></li>
     <li><a href="/potholes.php?page=">4</a></li>
     <li><a href="/potholes.php?page=">5</a></li>
-    <li><a href="/potholes.php?page=">Next</a></li>
+<?php if ($pagination['currentPage'] < $pagination['pages']) {?>
+    <li><a href="/potholes.php?page=<?php echo ($pagination['currentPage'] + 1); ?>">Next</a></li>
+<?php }?>
   </ul>
 </div>
 <?php
