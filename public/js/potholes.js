caa1704a25cda03eed890b150cdfe90c9ec7e214
kmonaghan <karl.t.monaghan@gmail.com>
2013-03-30 14:12:02 +0100

    added map with pins

---
+++ public/js/potholes.js
@@ -0,0 +1,47 @@
+var marker;
+var map;
+var infowindow = null;
+
+$(function() {
+//	info_window = new google.maps.InfoWindow();
+
+	infowindow = new google.maps.InfoWindow({
+                content: "loading..."
+        });
+
+	var mapOptions = {
+	          center: new google.maps.LatLng(53.4239331, -7.940689799999973),
+	          zoom: 7,
+	          mapTypeId: google.maps.MapTypeId.ROADMAP
+	        };
+	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	
+	if (latlngs.length)
+	{
+		var total = latlngs.length;
+		var latlngbounds = new google.maps.LatLngBounds();
+		
+		for (var i = 0; i < total; i++)
+		{ 
+			var currentLatLng = new google.maps.LatLng(latlngs[i].lat, latlngs[i].lng);
+
+			var marker = new google.maps.Marker({
+				position: currentLatLng,
+				map:map,
+				animation: google.maps.Animation.DROP,
+			});
+
+			marker.note = '<img src="/uploads/150x150_' + latlngs[i].imagename + '" /><a href="/pothole.php?pothole_id=' + latlngs[i].id + '">View</a>';
+
+			google.maps.event.addListener(marker, 'click', function() {
+				infowindow.content = this.note;
+				infowindow.open(map,this);
+			});
+
+			latlngbounds.extend(currentLatLng);
+		}
+
+		map.setCenter(latlngbounds.getCenter());
+		map.fitBounds(latlngbounds); 
+	}
+});
--- public/potholes.php
+++ public/potholes.php
@@ -9,50 +9,64 @@ $mapper = new PotholeMapper();
 $potholes = $mapper->getAll($page, $perPage);
 $pagination = $mapper->getPagination($page, $perPage);
 
+$latlngs = array();
 if ($potholes)
 {
 ?>
-<ul class="thumbnails">
+<div class="row-fluid">
+	<div class="span8">
+		<ul class="thumbnails">
 <?php
 	foreach($potholes as $pothole)
 	{
 		$image = $pothole->getFirstImage();
 ?>
-	<li>
-		<div class="thumbnail">
-			<div class="pagination-centered">
-				<a href="/uploads/<?php echo $image->get('filename');?>"><img data-src="holder.js/300x300" alt="" src="/uploads/300x300_<?php echo $image->get('filename');?>"></a>
+		<li>
+			<div class="thumbnail">
+				<div class="pagination-centered">
+					<a href="/uploads/<?php echo $image->get('filename');?>"><img data-src="holder.js/300x300" alt="" src="/uploads/300x300_<?php echo $image->get('filename');?>"></a>
+				</div>
+				<h5>Reported by <?php echo $pothole->get('nickname')?> on the <?php echo date('d/m/Y', $pothole->get('report_date')); ?></h5>
+				<p><?php echo $pothole->get('description')?></p>
+				<a href="/pothole.php?pothole_id=<?php echo $pothole->get('pothole_id'); ?>" class="btn btn-primary">View</a>
 			</div>
-			<h5>Reported by <?php echo $pothole->get('nickname')?> on the <?php echo date('d/m/Y', $pothole->get('report_date')); ?></h5>
-			<p><?php echo $pothole->get('description')?></p>
-			<a href="/pothole.php?pothole_id=<?php echo $pothole->get('pothole_id'); ?>" class="btn btn-primary">View</a>
-		</div>
-	</li>
+		</li>
 <?php
+		$latlngs[] = array('lat' => $pothole->get('lat'), 'lng' => $pothole->get('lng'), 'id' => $pothole->get('pothole_id'), 'imagename' => $image->get('filename'));
 	}	
 ?>
-</ul>
+		</ul>
 <?php 
 	if ($pagination['pages'] > 1)
 	{
 ?>
-<div class="pagination pagination-centered">
-  <ul>
+		<div class="pagination pagination-centered">
+			<ul>
 <?php if ($pagination['currentPage'] > 2) {?>
-    <li><a href="/potholes.php?page=<?php echo ($pagination['currentPage'] - 1); ?>">Prev</a></li>
+    				<li><a href="/potholes.php?page=<?php echo ($pagination['currentPage'] - 1); ?>">Prev</a></li>
 <?php }?>
-    <li><a href="/potholes.php?page=">1</a></li>
-    <li><a href="/potholes.php?page=">2</a></li>
-    <li><a href="/potholes.php?page=">3</a></li>
-    <li><a href="/potholes.php?page=">4</a></li>
-    <li><a href="/potholes.php?page=">5</a></li>
+    				<li><a href="/potholes.php?page=">1</a></li>
+    				<li><a href="/potholes.php?page=">2</a></li>
+    				<li><a href="/potholes.php?page=">3</a></li>
+    				<li><a href="/potholes.php?page=">4</a></li>
+    				<li><a href="/potholes.php?page=">5</a></li>
 <?php if ($pagination['currentPage'] < $pagination['pages']) {?>
-    <li><a href="/potholes.php?page=<?php echo ($pagination['currentPage'] + 1); ?>">Next</a></li>
+    				<li><a href="/potholes.php?page=<?php echo ($pagination['currentPage'] + 1); ?>">Next</a></li>
 <?php }?>
-  </ul>
-</div>
+			</ul>
+		</div>
 <?php
 	}
+?>
+	</div>
+        <div class="span4">
+                <div id="map_canvas" class="potholes-map"></div>
+        </div>
+</div>
+<script>
+var latlngs = <?php echo json_encode($latlngs); ?>;
+</script>
+<?php
 }
-
+$js = array('potholes');
 include 'footer.php';
