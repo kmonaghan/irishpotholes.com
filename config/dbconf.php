0c30fedfd51e72e521f4a85dc95d3739f9e0c80c
Baz <the.ewok@gmail.com>
2013-04-09 14:13:36 -0600

    Stupid mistake. Removing config from gitignore

---
+++ config/dbconf.php
@@ -0,0 +1,8 @@
+<?php
+
+define('MYSQLI_HOST', 'localhost');
+define('MYSQL_PORT', 3306);
+define('MYSQL_DATABASE', 'DATABASE**');
+define('MYSQL_USER', 'USERNAME**');
+define('MYSQL_PASSWORD', '');
+
