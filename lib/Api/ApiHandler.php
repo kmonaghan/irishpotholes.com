5abcd8b759f911f85a4f54fc17b1a63d1aafc3d2
Baz <the.ewok@gmail.com>
2013-04-13 12:10:33 -0600

    Incorporating code review changes

--- lib/Api/ApiHandler.php
+++ lib/Api/ApiHandler.php
@@ -8,6 +8,8 @@ class ApiHandler
     private $get;
     private $isPost;
     private $result;
+    private $success = true;
+    private $status = 200;
 
     public function __construct() 
     {
@@ -39,7 +41,9 @@ class ApiHandler
             $this->handlePotholeGet($query);
             break;
         default:
-            $this->result = array('success'=>false,'error'=>'Unknown Action');
+            $this->success =  false;
+            $this->status = 404;
+            $this->result = array('error'=>'Unknown Action');
             $this->renderJson();
         }
     }
@@ -49,31 +53,37 @@ class ApiHandler
         if(count($query)==1) {
             //It's a get all
             $potholeMapper = new \Pothole\PotholeMapper();
+
             $page = isset($this->get['page']) ? $this->get['page'] : 0;
             $perPage = isset($this->get['perPage']) ? $this->get['perPage'] : 20;
+
+            $perPage = max($perPage,50);
+
             $potholes = $potholeMapper->getAll($page,$perPage);
 
-            $result = '{"success":true,"result":[';
+            $result = array();
 
             foreach($potholes as $pothole) {
-                $result .= $pothole->toJSON();
-                $result .= ',';
+                $result[] = $pothole->getPublic();
             }
 
-            $result = trim($result,',');
-
-            $result .= ']}';
-
             $this->result = $result;
-            $this->renderJson(false);
 
         } else if(count($query)==2) {
-            $pothole = new \Pothole\Pothole($query[1]);
-            $this->result = '{"success":true,"result":['.$pothole->toJSON().']}';
-            $this->renderJson(false);
+            $pothole = new \Pothole\Pothole((int)$query[1]);
+            if($pothole->get('pothole_id')!==false) { 
+                $this->result = $pothole->getPublic();
+            } else {
+                $this->result = array();
+            }
+        } else {
+            $this->result = array();
         }
 
+        $this->renderJson();
+
     }
+
 
     private function getQueryAsArray() 
     {
@@ -125,24 +135,72 @@ class ApiHandler
             $image = $imageMapper->getByName($filename);
             $image->delete();
             
-            //Delete from file system
-            $mask = UPLOAD_DIR.DIRECTORY_SEPARATOR."*".$filename;
-            array_map( "unlink", glob( $mask ) );
+            
         }
         $this->renderJson();
     }
 
-    private function renderJson($encode=true)
+    private function renderJson()
     {
-        if(!$this->result['success']) {
-            header('HTTP/1.0 500 Internal Server Error');
-        } 
-
-        if($encode) {
-            echo json_encode($this->result);
-        } else {
-            echo $this->result;
-        }
+        $this->sendHeaders();
+        echo json_encode(array('success'=>$this->success,'result'=>$this->result));
     }
+
+    private function sendHeaders() {
+        header('Cache-Control: no-cache, must-revalidate');
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+        header('Content-type: application/json');
+        $this->sendHTTPStatus();
+    }
+
+
+    private function sendHTTPStatus() 
+    {
+        $http = array(
+            100 => 'HTTP/1.1 100 Continue',
+            101 => 'HTTP/1.1 101 Switching Protocols',
+            200 => 'HTTP/1.1 200 OK',
+            201 => 'HTTP/1.1 201 Created',
+            202 => 'HTTP/1.1 202 Accepted',
+            203 => 'HTTP/1.1 203 Non-Authoritative Information',
+            204 => 'HTTP/1.1 204 No Content',
+            205 => 'HTTP/1.1 205 Reset Content',
+            206 => 'HTTP/1.1 206 Partial Content',
+            300 => 'HTTP/1.1 300 Multiple Choices',
+            301 => 'HTTP/1.1 301 Moved Permanently',
+            302 => 'HTTP/1.1 302 Found',
+            303 => 'HTTP/1.1 303 See Other',
+            304 => 'HTTP/1.1 304 Not Modified',
+            305 => 'HTTP/1.1 305 Use Proxy',
+            307 => 'HTTP/1.1 307 Temporary Redirect',
+            400 => 'HTTP/1.1 400 Bad Request',
+            401 => 'HTTP/1.1 401 Unauthorized',
+            402 => 'HTTP/1.1 402 Payment Required',
+            403 => 'HTTP/1.1 403 Forbidden',
+            404 => 'HTTP/1.1 404 Not Found',
+            405 => 'HTTP/1.1 405 Method Not Allowed',
+            406 => 'HTTP/1.1 406 Not Acceptable',
+            407 => 'HTTP/1.1 407 Proxy Authentication Required',
+            408 => 'HTTP/1.1 408 Request Time-out',
+            409 => 'HTTP/1.1 409 Conflict',
+            410 => 'HTTP/1.1 410 Gone',
+            411 => 'HTTP/1.1 411 Length Required',
+            412 => 'HTTP/1.1 412 Precondition Failed',
+            413 => 'HTTP/1.1 413 Request Entity Too Large',
+            414 => 'HTTP/1.1 414 Request-URI Too Large',
+            415 => 'HTTP/1.1 415 Unsupported Media Type',
+            416 => 'HTTP/1.1 416 Requested Range Not Satisfiable',
+            417 => 'HTTP/1.1 417 Expectation Failed',
+            500 => 'HTTP/1.1 500 Internal Server Error',
+            501 => 'HTTP/1.1 501 Not Implemented',
+            502 => 'HTTP/1.1 502 Bad Gateway',
+            503 => 'HTTP/1.1 503 Service Unavailable',
+            504 => 'HTTP/1.1 504 Gateway Time-out',
+            505 => 'HTTP/1.1 505 HTTP Version Not Supported',
+        );
+     
+        header($http[$this->status]);
+    }
+
 
 }
